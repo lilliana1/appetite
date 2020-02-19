0a6c13235aaa42c8d1b6d8ba4c983411561ac334
Lilliana Ramos <lilliana.cramos@gmail.com>
2020-02-19 09:06:55 -0500

    added card

---
+++ client/src/components/Review/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Review() {
+    return (
+        <div className="card">
+  <img className="card-img-top" src="https://blog.cws.net/hubfs/blog-files/blog-cws-google-review.png" alt="Card image cap" />
+  <div className="card-body">
+    <h5 className="card-title">Details</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <div className="card-body">
+    <h5 className="card-title">Review</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+    
+    );
+}
+
+export default Review;
--- client/src/pages/Details.js
+++ client/src/pages/Details.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Instagram from "../components/Instagram";
 import Map from "../components/Map";
+import Review from "../components/Review";
 
 function Details() {
     return (
         <div>
         <h1 >Name of place + in +  City </h1>
         <Instagram />  
+        <Review />
         <Map />
         </div>
         
