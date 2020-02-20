673b6ab12710c7d2f9a8378fa38e968769e48b78
Lilliana Ramos <lilliana.cramos@gmail.com>
2020-02-20 15:09:33 -0500

    images are appearing and table is at middle of the page

--- client/src/components/RestaurantInfo/index.js
+++ client/src/components/RestaurantInfo/index.js
@@ -1,24 +1,29 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import "./style.css";
+
+
 
 function RestaurantInfo() {
     return (
-
-        <table className="table">
+        <div>
+        <table align="center" className="table">
             <thead>
                 <tr>
-                    <th scope="col">image</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Info</th>
+                    <th scope="col"></th>
+                    <th scope="col">Restaurant</th>
+                    <th scope="col">i</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <th scope="row">image.png</th>
+                    <th scope="row"><img></img></th>
                     <td>Vianda</td>
-                    <td>i</td>
+                    <td><Link to="/details">i</Link></td>
                 </tr>
             </tbody>
         </table>
+        </div>
     );
 }
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import SearchBar from '../components/SearchBar';
 import API from "../utils/API";
 import "../components/Checkbox";
+import { Link } from 'react-router-dom';
+import RestaurantInfo from "../components/RestaurantInfo";
 
 
 class Home extends React.Component {
@@ -62,33 +64,63 @@ render(){
             handleChange={this.handleChange}
             handleClick={this.handleClick}
             />
+            <div className="row">
+                <div className="col-2 categories">
+                <h3 className="categories">Categories</h3>
+                {/* <button type="submit" >click me </button> */}
 
-            <div className="categories">
-            <h3 className="categories">Categories</h3>
-            {/* <button type="submit" >click me </button> */}
-
-                    <div>    
-                    <form className="categories">
-                        <button id="categoriesSearchButton">Search</button>
-                        {this.state.categories.map(category => {
-
+                        <div>    
+                        <form className="categories">
+                            <button id="categoriesSearchButton">Search</button>
+                            {this.state.categories.map(category => {
                             
-                            return (
-                            <div>
-                            <input className="form-check-input" type="checkbox" value={category} id="defaultCheck1" />
-                            <label className="form-check-label" for="defaultCheck1">
-                            {category}
-                            </label>
-                            </div>
+                                return (
+                                <div>
+                                <input className="form-check-input" type="checkbox" value={category} id="defaultCheck1" />
+                                <label className="form-check-label" for="defaultCheck1">
+                                {category}
+                                </label>
+                                </div>
 
-                        )
-                            })}
-                        </form>
-                    {/* <button>{item.categories[0].title}</button> */}
-                    {/* <img src={item.image_url} alt="test" /> */}
-                    </div>
+                            )
+                                })}
+                            </form>
+                            
+                        {/* <button>{item.categories[0].title}</button> */}
+                        {/* <img src={item.image_url} alt="test" /> */}
+                        </div>
+                        
+
+                </div>
+                <div className="col-6">
+                {/* <RestaurantInfo /> */}
+                <table align="center" className="table">
+            <thead>
+                <tr>
+                    <th scope="col"></th>
+                    <th scope="col">Restaurant</th>
+                    <th scope="col">i</th>
+                </tr>
+            </thead>
+            <tbody>
+                {this.state.apiData.map(item => {
+                    return(
+                        <tr>
+                    <th scope="row"><img src={item.image_url} atl={item.name} /></th>
+                    <td>{item.name}</td>
+                    <td><Link to="/details">i</Link></td>
+                </tr>
+
+                    )
+                })}
                 
+            </tbody>
+        </table>
 
+
+
+
+                </div>
             </div>
         </div>
         
