ff47505b51c11de86b50c73402e51d8f1cdb5a8f
Lilliana Ramos <lilliana.cramos@gmail.com>
2020-03-02 18:08:06 -0500

    logo at top of page

--- client/src/components/Logo/index.js
+++ client/src/components/Logo/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import appetite from "./appetite.png"
+import "./style.css";
 
 export default function Logo() {
     return (
         <div>
-            <img src={appetite} />
+            <img id="appetiteLogo" src={appetite} />
         </div>
     )
 }
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,12 +4,13 @@ import Logo from '../components/Logo';
 function About() {
     return (
         <div>
-            <h1>
+            <div>
             <Logo />
-            </h1>
+            </div>
+            
             <br />
             {/* STORY */}
-            <h3>Story</h3>
+            <h3>About Appetite.</h3>
             <p>
                 Imagine you just got to a new city and don’t know anything about it when it comes to eat! What’s the point of searching on an engine and noticing that places are fast foods (why eat fast food or big food national food chains when there are so many places to eat great food?), close or too far away from you? Appetite. is here to make that browsing experience unique.
             </p>
@@ -20,13 +21,13 @@ function About() {
             Stop wasting hours in a new city when you are searching for a place to eat. Appetite. is here to help you make that search way easier by filtering restaurants where you can have new experiences and great food near you!
             </p>
 
-            {/* API USED */}
             <br />
             <h4>How's behind the creation of Appetite.?</h4>
             <p>
-                Lilliana Ramos
+            Lilliana Ramos created Appetite. in 2020 with the mission to help foodies search places to eat that are open at the time of searching. 
             </p>
             <br />
+            {/* API USED */}
             <h5>API</h5>
             <p>
             Appetite. use Yelp's amazing API.
